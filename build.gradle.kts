44a5e9f6ab959477fb3600ad3b4f86efd5c9cbca
stone-wall <stonewall@pklite.xyz>
2021-07-13 05:16:51 -0400

    quick fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.virginiaprivacy"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -27,7 +27,7 @@ publishing {
         create<MavenPublication>("release") {
             group = "com.virginiaprivacy.adsbtrack.drivers"
             artifactId = "sdr"
-            version = "0.1.0"
+            version = "0.1.1"
             from(components["java"])
         }
     }
