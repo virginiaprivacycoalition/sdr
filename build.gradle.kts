50066829176424b11818b3dccddcb6417f50d3e8
Bob Dole <virginiaprivacy@protonmail.com>
2021-07-28 03:58:04 +0800

    remove BigInteger usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.virginiaprivacy"
-version = "0.1.3"
+version = "0.1.4"
 
 repositories {
     mavenCentral()
@@ -27,7 +27,7 @@ publishing {
         create<MavenPublication>("release") {
             group = "com.virginiaprivacy.adsbtrack.drivers"
             artifactId = "sdr"
-            version = "0.1.3"
+            version = "0.1.4"
             from(components["java"])
         }
     }
